fbaf0351d520de6e113778ec54262689284a0d9f
4lexxoranges <76011237+unn4meDd@users.noreply.github.com>
2021-03-24 21:49:54 +0300

    added code

--- Task3/Task3/Program.cs
+++ Task3/Task3/Program.cs
@@ -6,7 +6,22 @@ namespace Task3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введите любую строку: ");
+            string userInput = Console.ReadLine();
+            char symbol = ' ';
+
+            int length = userInput.Length;
+            char[] stringArray = userInput.ToCharArray();
+            int wordCounter = 0;
+            for (int i = 0; i < length; i++)
+            {
+                if (stringArray[i] == symbol)
+                {
+                    wordCounter++;
+                }
+            }
+            Console.WriteLine($"Количество слов: {wordCounter + 1}");
+            Console.ReadLine();
         }
     }
 }
