c9716f0d64aefea55868cf5b8ab4e891d45fdf86
4lexxoranges <76011237+unn4meDd@users.noreply.github.com>
2021-03-24 21:36:30 +0300

    added new projects and code

---
+++ Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку любой длинны: ");
+            string userInput = Console.ReadLine();
+            Console.Write("Введите любой символ, для того чтобы подсчитать процент вхождения данного символа: ");
+            char symbol = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            int length = userInput.Length;
+            char[] stringArray = userInput.ToCharArray();
+            int counter = 0;
+            for (int i = 0; i < length; i++)
+            {
+                if(stringArray[i] == symbol)
+                {
+                    counter++;
+                }
+            }
+            char quotes = '"';
+            double lengthDouble = Convert.ToDouble(length);
+            double counterDouble = Convert.ToDouble(counter);
+            double percentage = (counterDouble / 100) * lengthDouble;
+            Console.WriteLine($"Процент вхождения символа - {quotes}{symbol}{quotes}, равен - {percentage}");
+        }
+    }
+}
---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 2, 8, 3, 9, 0, 30, 1 };
+            int maxNumber = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if(array[i] > maxNumber)
+                {
+                    maxNumber = array[i];
+                }
+            }
+            Console.WriteLine(maxNumber);
+        }
+    }
+}
