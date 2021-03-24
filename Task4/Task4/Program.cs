bcc32b6dd1813d1b2fec4419bccfd869eba4d7d2
4lexxoranges <76011237+unn4meDd@users.noreply.github.com>
2021-03-24 22:49:43 +0300

    add new project

---
+++ Task4/Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 7, 1, 8, 9, 4};
+            int maxNumber = 0, minNumber = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if(numbers[i] > maxNumber)
+                {
+                    maxNumber = numbers[i];
+                }
+                else if(numbers[i] == numbers[2])
+                {
+                    minNumber = numbers[2];
+                }
+            }
+            Console.Write($"Разница между максимальным - {maxNumber}, и минимальным числом - {minNumber}, равна - {maxNumber - minNumber}");
+            Console.ReadKey();
+        }
+    }
+}
