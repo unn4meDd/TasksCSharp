ce889de4a0aab3e6ff2e1ea37f5ea280ff7e71d8
4lexxoranges <76011237+unn4meDd@users.noreply.github.com>
2021-03-31 20:39:20 +0300

    added new script

---
+++ Task12/Task12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[100];
+            int counter = 0;
+            Random rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(-300, 300);
+                Console.WriteLine(array[i]);
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                if(0 < array[i] < 125)
+                {
+                    counter++;
+                }
+            }
+        }
+    }
+}
---
+++ Task13/Task13/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = { 
+                { 6, 5, 3, 8, 7},
+                { 3, 1, 10, 6, 8},
+                { 4, 2, 5, 8, 20},
+                { 7, 3, 4, 9, 5},
+                { 6, 2, 5, 10, 15},
+                { 4, 7, 12, 18, 19},
+                { 20, 1, 6, 10, 11},
+                { 1, 12, 10, 12, 6},
+                { 18, 12, 19, 4, 2},
+                { 15, 5, 11, 2, 14}
+            };
+            int sum = 0;
+            Random rand = new Random();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    sum += array[i, j];
+                }
+                Console.WriteLine(sum + " ");
+            }
+        }
+    }
+}
