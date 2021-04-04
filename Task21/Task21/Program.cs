518772ecae3dd9a881d65d92b8816a295f85c789
4lexxoranges <76011237+unn4meDd@users.noreply.github.com>
2021-04-04 15:38:27 +0300

    Update Program.cs

--- Task21/Task21/Program.cs
+++ Task21/Task21/Program.cs
@@ -6,7 +6,36 @@ namespace Task21
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var a = new[] { 1, 2, 3, 4, 5 };
+            var b = new[] { 5, 4, 3, 2, 1 };
+            var c = new[] { 1, 3, 2, 5, 4 };
+            PrintCheck(a);
+            PrintCheck(b);
+            PrintCheck(c);
+
+            static bool Check(int[] array, bool ask)
+            {
+                for (int i = 0; i < array.Length - 1; i++)
+                    if ( (ask && (array[i] > array[i + 1])) || (!ask && (array[i] < array[i + 1])) ) 
+                        return false;
+                return true;
+            }
+
+            static void PrintCheck(int[] a)
+            {
+                if (Check(a, true))
+                {
+                    Console.WriteLine("Упорядочен по возрастанию");
+                }
+                else if (Check(a, false))
+                {
+                    Console.WriteLine("Упорядочен по убыванию");
+                }
+                else
+                {
+                    Console.WriteLine("Не упорядочен");
+                }
+            }
         }
     }
 }
